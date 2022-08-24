d91ed684fd3040bad446ba676fc5ca0835f83137
Jay Flanary <j.c.flanary@gmail.com>
2022-08-24 14:39:44 -0400

    removed testDb code

--- server/_db/db.js
+++ server/_db/db.js
@@ -1,9 +1,6 @@
 const Sequelize = require("sequelize");
 const pkg = require('../../package.json');
 
-// // const testDb = new Sequelize(
-// //   process.env.DATABASE_URL || "postgres://postgres:Bigman<26@localhost:5432/zoneTwoTest"
-// // );
 const databaseName = pkg.name + (process.env.NODE_ENV === "test" ? "Test" : "");
 
 const db = new Sequelize(
