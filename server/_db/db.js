6a644b84348a9808bcb4a0bbdd5bea16779db006
Jay Flanary <j.c.flanary@gmail.com>
2022-08-24 14:28:59 -0400

    test db composition
    œ
    q
    œ
    Q
    ??
    
    /
    \
    Q
    
    \

--- server/_db/db.js
+++ server/_db/db.js
@@ -1,14 +1,14 @@
 const Sequelize = require("sequelize");
+const pkg = require('../../package.json');
 
-// const testDb = new Sequelize(
-//   process.env.DATABASE_URL || "postgres://postgres:Bigman<26@localhost:5432/zoneTwoTest"
-// );
+// // const testDb = new Sequelize(
+// //   process.env.DATABASE_URL || "postgres://postgres:Bigman<26@localhost:5432/zoneTwoTest"
+// // );
+const databaseName = pkg.name + (process.env.NODE_ENV === "test" ? "Test" : "");
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || "postgres://postgres:Bigman<26@localhost:5432/zoneTwo"
+  process.env.DATABASE_URL || `postgres://postgres:Bigman<26@localhost:5432/${databaseName}`
 );
 
-module.exports = {
-  db
-// testDb
-};
+
+module.exports = db;
