f78881ae972707386ff48c60ec6128554a6f8cd3
Jay Flanary <j.c.flanary@gmail.com>
2022-08-24 15:41:50 -0400

    added workout model

--- server/_db/index.js
+++ server/_db/index.js
@@ -1,6 +1,8 @@
 const db = require('./db');
 
 const User = require('./models/User');
+// const { User, Workout } = require('./models');
+const Workout = require('./models/Workout')
 
 // associations go here
 
