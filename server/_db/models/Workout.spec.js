ab250edba0b2a4ed7a68547aed811198fb98e0df
Jay Flanary <j.c.flanary@gmail.com>
2022-08-24 15:42:30 -0400

    created workouts test

---
+++ server/_db/models/Workout.spec.js
@@ -0,0 +1,28 @@
+const { expect } = require('chai');
+const seed = require('../../../script/seed');
+const { db, models: { Workout } } = require('../../_db');
+
+describe('Workout Model', ()=> {
+  let workouts;
+  beforeEach(async() =>{
+    workouts = (await seed()).workouts;
+  });
+
+  describe('Workout Data Layer', ()=> {
+    describe('Workout Check', ()=> {
+      it('There are 2 workouts', async()=> {
+        expect(workouts.length).to.equal(2);
+      });
+      it('First workout has name', async()=> {
+        expect(workouts[0].name).to.be.ok;
+      });
+      it('First workout has description', async()=> {
+        expect(workouts[0].description).to.be.ok;
+      });
+      it('First workout has data uploaded', async()=> {
+        expect(workouts[0].data).to.be.ok;
+      });
+    });
+  });
+
+});
