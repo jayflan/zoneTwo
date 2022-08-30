4166b010c6e3f01dd1fd69ea5093658c35087a7a
Jay Flanary <j.c.flanary@gmail.com>
2022-08-30 12:27:48 -0400

    api setup

--- server/app.js
+++ server/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
 
 const app = express();
 
 // static middleware
 // middleware
+app.use(morgan("dev"));
 // api/router
+app.use("/api", require("./_api"));
+//auth routes
+
 // error handling
 app.use((req, res, next) => {
   const error = Error('Page Not Found.');
