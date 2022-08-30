47de6df2129939492eda0c004a2ae88dfd08b236
Jay Flanary <j.c.flanary@gmail.com>
2022-08-30 10:54:17 -0400

    add logging config

--- server/_db/db.js
+++ server/_db/db.js
@@ -3,8 +3,17 @@ const pkg = require('../../package.json');
 
 const databaseName = pkg.name + (process.env.NODE_ENV === "test" ? "Test" : "");
 
+const config = {
+  logging: false
+};
+// turn on logging via env script
+if(process.env.LOGGING === "true") {
+  delete config.logging;
+};
+
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://postgres:Bigman<26@localhost:5432/${databaseName}`
+  process.env.DATABASE_URL || `postgres://postgres:Bigman<26@localhost:5432/${databaseName}`,
+  config
 );
 
 
