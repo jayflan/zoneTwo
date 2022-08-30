d75e01154f64efe5814594d721abcdc7f84b3e8d
Jay Flanary <j.c.flanary@gmail.com>
2022-08-30 10:54:45 -0400

    update relationships

--- server/_db/index.js
+++ server/_db/index.js
@@ -1,15 +1,21 @@
 const db = require('./db');
 
 const User = require('./models/User');
-// const { User, Workout } = require('./models');
-const Workout = require('./models/Workout')
+const Workout = require('./models/Workout');
+const Bike = require('./models/Bike');
 
 // associations go here
+User.hasMany(Bike);
+Bike.belongsTo(User);
+
+User.hasMany(Workout);
+Workout.belongsTo(User);
 
 module.exports = {
   db,
   models: {
     User,
     Workout,
+    Bike
   },
 };
