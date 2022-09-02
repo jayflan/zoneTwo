10439b070ddca7b70df03caec5f51d2e413dd83d
Jay Flanary <j.c.flanary@gmail.com>
2022-09-02 17:17:02 -0400

    changed Routes comp to Landing comp

--- client/App.js
+++ client/App.js
@@ -1,23 +1,25 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react"; "@chakra-ui/react";
+import { ChakraProvider, Container } from "@chakra-ui/react";
 import Navbar from "./_components/Navbar";
-import Routes from "./_components/Routes";
+import Landing from "./_components/Landing";
 import Footer from "./_components/Footer";
 
 const App = () => {
   return(
     <ChakraProvider>
-      <div className="main">
-        <nav>
-          <Navbar />
-        </nav>
-        <div>
-          <Routes />
+      <Container maxW='container.xl' p='0'>
+        <div className="main">
+          <nav>
+            <Navbar />
+          </nav>
+          <div>
+            <Landing />
+          </div>
+          <div>
+            <Footer />
+          </div>
         </div>
-        <div>
-          <Footer />
-        </div>
-      </div>
+      </Container>
     </ChakraProvider>  
   );
 };
