d0d9e0eb59dff303cfc93d7e92593f1935444991
Jay Flanary <j.c.flanary@gmail.com>
2022-09-07 13:27:44 -0400

    initial react router dom setup

--- client/App.js
+++ client/App.js
@@ -1,23 +1,26 @@
 import React from "react";
 import { ChakraProvider, Container } from "@chakra-ui/react";
 import Navbar from "./_components/Navbar";
-import Landing from "./_components/Landing";
-import Footer from "./_components/Footer";
+import ReactRoutes from "./ReactRoutes";
 
 const App = () => {
   return(
     <ChakraProvider>
       <Container maxW='container.xl' p='0'>
         <div className="main">
-          <nav>
+          <Navbar/>
+          <ReactRoutes />
+          {/* <nav>
             <Navbar />
           </nav>
           <div>
-            <Landing />
-          </div>
-          <div>
+            <LogIn />
+            <Footer /> */}
+            {/* <Landing /> */}
+          {/* </div> */}
+          {/* <div>
             <Footer />
-          </div>
+          </div> */}
         </div>
       </Container>
     </ChakraProvider>  
