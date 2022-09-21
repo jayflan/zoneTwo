14a8c05f4dcf05146bc70d7e65cdee2dfec61ef7
Jay Flanary <j.c.flanary@gmail.com>
2022-09-21 15:03:06 -0400

    add redux provider wrapper

--- client/App.js
+++ client/App.js
@@ -2,28 +2,21 @@ import React from "react";
 import { ChakraProvider, Container } from "@chakra-ui/react";
 import Navbar from "./_components/Navbar";
 import ReactRoutes from "./ReactRoutes";
+import { Provider } from "react-redux";
+import store from "../client/_store"
 
 const App = () => {
   return(
-    <ChakraProvider>
-      <Container maxW='container.xl' p='0'>
-        <div className="main">
-          <Navbar/>
-          <ReactRoutes />
-          {/* <nav>
-            <Navbar />
-          </nav>
-          <div>
-            <LogIn />
-            <Footer /> */}
-            {/* <Landing /> */}
-          {/* </div> */}
-          {/* <div>
-            <Footer />
-          </div> */}
-        </div>
-      </Container>
-    </ChakraProvider>  
+    <Provider store={store}>
+      <ChakraProvider>
+        <Container maxW='container.xl' p='0'>
+          <div className="main">
+            <Navbar/>
+            <ReactRoutes />
+          </div>
+        </Container>
+      </ChakraProvider>  
+    </Provider>
   );
 };
 
