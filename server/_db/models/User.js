9a6567ebd1f20d3a20053f88296f031a4bd51b4a
Jay Flanary <j.c.flanary@gmail.com>
2022-10-19 12:24:47 -0400

    jsonWebToken setup

--- server/_db/models/User.js
+++ server/_db/models/User.js
@@ -1,6 +1,7 @@
 const Sequelize = require("sequelize");
 const { STRING, UUID, UUIDV4 } = Sequelize;
 const db = require("../db");
+const jwt = require("jsonwebtoken");
 
 const User = db.define("user", {
   id: {
@@ -24,3 +25,41 @@ const User = db.define("user", {
 );
 
 module.exports = User;
+
+//<----- instanceMethods ----->
+
+// User.prototype.correctPassword = function (candidatePwd) {
+//   return candidatePwd === this.password
+// };
+
+User.prototype.generateToken = function () {
+  return jwt.sign({ id: this.id }, process.env.JWT);
+};
+
+//<----- classMethods ----->
+
+User.authenticate = async function ({ email, password }) {
+  const user = await this.findOne({ where: { email }});
+  //todo add password eval here
+  if(!user /* or correctPassword*/) {
+    const error = Error("Incorrect username/password");
+    error.status = 401;
+    throw error;
+  };
+  return user.generateToken();
+};
+
+User.findByToken = async function (token) {
+  try {
+    const { id } = await jwt.verify(token, process.env.JWT);
+    const user = User.findByPk(id);
+    if(!user) {
+      throw "nooo";
+    };
+    return user;
+  } catch (ex) {
+    const error =Error("bad token");
+    error.status = 401;
+    throw error;
+  };
+};
