40fcaf6f2c0f595678cfa3c2b0de11f00eb60b31
Jay Flanary <j.c.flanary@gmail.com>
2022-10-21 14:52:28 -0400

    for auth express route setup & testing

---
+++ server/_auth/index.js
@@ -0,0 +1,41 @@
+const router = require("express").Router();
+const { models: { User } } = require("../_db");
+const app = require("../app");
+module.exports = router;
+
+//logout auth GET to pass logout method
+router.get("/logout",(req, res) => {
+  //todo logout() is for Passport, which you aren't using!!!
+  //todo may only need logout via frontend? (destroy token)
+  // req.logout();
+  res.redirect("/");
+});
+//login auth POST to pass User authenticate method (generateJWT)
+router.post("/login", async (req, res, next)=>{
+  try {
+    res.send({ token: await User.authenticate(req.body) });
+  } catch(err) {
+    next(err);
+  };
+});
+//signup auth POST to create new user & generateJWT
+router.post("/signup", async (req, res, next)=> {
+  try {
+    const user = await User.create(req.body);
+    res.send({ token: await user.generateToken() });
+  } catch(err) {
+    if(err.name === "SequelizeUniqueConstraintError") {
+      res.status(401).send("User already exists");
+    } else {
+      next(err);
+    };
+  };
+});
+//user 'me' auth to GET user db data
+router.get("/me", async (req, res, next)=> {
+  try {
+    res.send(await User.findByToken(req.headers.authorization))
+  } catch(err) {
+    next(err);
+  }
+});
