0b158cbcd33c889ff6a5ee139004b8e4ca4c5348
Jay Flanary <j.c.flanary@gmail.com>
2022-11-03 18:14:43 -0400

    added auth to LogIn

--- client/_components/LogIn.js
+++ client/_components/LogIn.js
@@ -1,10 +1,48 @@
-import { Box, Center, Divider, Flex, VStack, Image, Text, Button } from "@chakra-ui/react";
-import React from "react";
-import { Link } from "react-router-dom";
+// import { Box, Center, Divider, Flex, VStack, Image, Text, Button, Input } from "@chakra-ui/react";
+import { Box, Center, Divider, Flex, FormControl, VStack, Image, Text, Button, Input } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector, useStore } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 import { PrimaryButton, SecondaryButton } from "./styles/index";
-
+import { authenticate, me } from "../_store/auth";
 
 const LogIn = () => {
+  
+//<------------------------------ hooks ------------------------------>//
+
+  //create local state hook for email & password input handling
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+const navigate = useNavigate();
+  //prep store-state
+const dispatch = useDispatch();
+const authUser = useSelector((state) => state.auth) || [];
+
+//<------------------------------ componentDidMount ------------------------------>//
+
+useEffect(() => {
+  dispatch(me());
+}, [!authUser]);
+
+//<------------------------------ event & error handling ------------------------------>//
+
+// const [errorMsg, setErrorMsg] = useState("");
+
+const handleEmailChange = (event) => {
+  setEmail(event);
+};
+
+const handlePasswordChange = (event) => {
+  setPassword(event);
+};
+
+const handleSubmit = (event) => {
+  dispatch(authenticate(email, password, event));
+  if(authUser) navigate("/Dashboard");
+};
+
+//<------------------------------ React render ------------------------------>//
+
   return(
     <div>
       <Flex
@@ -14,84 +52,45 @@ const LogIn = () => {
         <Center mt="20" mb="80">
           <Box bg="blackAlpha.600" borderRadius="sm">
             <Center as="h1" bg="blackAlpha.500" p="4" borderRadius="sm">
-              <Text /*noOfLines={[1,2]}*/ fontSize="4xl" color="white">
-                Log In
+              <Text fontSize="4xl" color="white">
+                Please Login
               </Text>
             </Center>
-            <VStack spacing='10' p='4' mt="4" mb="4">
-              <PrimaryButton w='300px'>Log In using Google</PrimaryButton>
-              <PrimaryButton w='300px'>Log In using Apple</PrimaryButton>
-              <Text /*noOfLines={[1,2]}*/ color="gray.400">
-                Or log in with your email
-              </Text>
-              <SecondaryButton w='300px'>Use my email</SecondaryButton>
+            <VStack spacing='5' p='4' mt="4" mb="4">
+              <PrimaryButton w='300px'>Login with Google</PrimaryButton>
+              <PrimaryButton w='300px'>Login with Apple</PrimaryButton>
+              <Text color="gray.400">or login with your email address</Text>
+
+              <FormControl>
+                <Center>
+                  <Input w="300px" color="white" placeholder="Email" _placeholder={{color: "white.100"}}
+                    onChange={(e)=>handleEmailChange(e.target.value)}
+                  ></Input>
+                </Center>
+              </FormControl>
+
+              <FormControl>
+                <Center>
+                  <Input w="300px" color="white" placeholder="Password" _placeholder={{color: "white.100"}}
+                    onChange={(e)=>handlePasswordChange(e.target.value)}
+                  ></Input>
+                </Center>
+              </FormControl>
+
+              <SecondaryButton w='300px' onClick={()=>{handleSubmit("login")}}>Login</SecondaryButton>
             </VStack>
             <Center bg="blackAlpha.500" p="4">
               <Text fontSize="m" color="white">
-                <Link to="/landing">Forget your password?</Link>
+                Not a member? <Link to="/signup">   Sign Up</Link>
               </Text>
             </Center>
           </Box>
         </Center>
       </Flex>
-
-      {/* <Box
-        w="100px"
-        h="100px"
-        bg="red"
-        pos="absolute"
-        top="0"
-        bottom="0"
-        left="0"
-        right="0"
-        margin="auto"
-      >Test Box</Box> */}
-
-
-{/* <Box pos="absolute">Cover</Box>
-<Box pos="absolute" top="50%" left="50%">
-  Absolute with top and left
-</Box>
-<Box pos="fixed" w="100%" zIndex={2}>
-  Fixed with zIndex
-</Box> */}
-      
-      
-      {/* <Box backgroundImage="url('/_img/roadBike.jpg')">
-        <Center>
-          <Box bg="gray.300" h="500px">
-            <Box bg="black" h="500px" w="100px" >
-              <VStack mt='20' spacing='10'>
-                <Button w='300px' borderRadius='0'>Sign Up with Google</Button>
-                <Button w='300px' borderRadius='0'>Sign Up with Apple</Button>
-                <Divider orientation="horizontal" w='300px'></Divider>
-                <Button w='300px' borderRadius='0'>Use my email</Button>
-              </VStack>
-            </Box>
-          </Box>
-        </Center>
-      </Box> */}
-      
-      
-      {/* <div style={{backgroundimage: url('_img/roadBike.jpg')}}> */}
-        {/* <Box bg='gray.100' h='full' w='full'>
-            <Image src='_img/roadBike.jpg' alt='Cycling'/>
-          </Box> */}
-
-        {/* <Center as='h1' p='10'><Text as='b' fontSize='xl'>The #1 app for this cyclist</Text></Center>
-        <Flex id='landing'>
-          <Box bg='gray.100' h='full' w='full'>
-            <Image src='_img/roadBike.jpg' alt='Cycling'/>
-          </Box>
-          <VStack mt='20' w='full' spacing='10'>
-            <Button w='300px' borderRadius='0'>Sign Up with Google</Button>
-            <Button w='300px' borderRadius='0'>Sign Up with Apple</Button>
-            <Divider orientation="horizontal" w='300px'></Divider>
-            <Button w='300px' borderRadius='0'>Use my email</Button>
-          </VStack>
-        </Flex> */}
-    </div>
+      </div>
   );
 };
+  
+  
 
 export default LogIn;
