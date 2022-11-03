13fd9f00a1dec27bf67b81ba9293c6a1eb549a9e
Jay Flanary <j.c.flanary@gmail.com>
2022-11-03 18:11:48 -0400

    add dashboard route

--- client/ReactRoutes.js
+++ client/ReactRoutes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Dashboard from "./_components/Dashboard";
 import Landing from "./_components/Landing";
 import LogIn from "./_components/LogIn";
 import SignUp from "./_components/SignUp";
@@ -11,6 +12,7 @@ const ReactRoutes = () => {
         <Route exact path='landing' element={<Landing/>}/>
         <Route exact path="login" element={<LogIn/>}/>
         <Route exact path="signup" element={<SignUp/>}/>
+        <Route exact path="dashboard" element={<Dashboard/>}/>
       </Routes>
     </div>
   );
