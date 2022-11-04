e2960e9de25df41170de0f88d4f8e762cfdd6f37
Jay Flanary <j.c.flanary@gmail.com>
2022-11-04 14:52:12 -0400

    add redux

--- client/_components/tests/Navbar.spec.js
+++ client/_components/tests/Navbar.spec.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Provider } from "react-redux";
+import store from "../../_store";
 import { BrowserRouter } from "react-router-dom";
 import { expect } from "chai";
 import { render, screen } from "@testing-library/react";
@@ -9,7 +11,12 @@ describe("Component Check - Navbar", () => {
 
   describe("<Navbar /> is active?", () => {
     it("renders the zoneTwo div text", () => {
-      render(<Navbar />, {wrapper: BrowserRouter});
+      render(
+        <Provider store={store}>
+          <Navbar />
+        </Provider>
+      , {wrapper: BrowserRouter}
+      );
       const message = screen.getByText('zoneTwo');
       expect(message).to.exist;
     })
