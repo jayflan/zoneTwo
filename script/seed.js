1329412917d5abd0ea10cc47fbfebc6e65cde71f
James Flanary <jflanmacpro@Jamess-MacBook-Pro.local>
2023-05-09 16:10:38 -0400

    updated with new function to append gpxData w/ distance data

--- script/seed.js
+++ script/seed.js
@@ -74,6 +74,28 @@ async function seed() {
     'minutes': 0,
     'hours': 0
   };
+  //create NEW gpxWorkout obj w/ appended data
+  //todo switch from gpxOutputDataMtn to a function that recreates array & accums distance over workout by trkpt, then append
+  
+  const appendToGpxData = (origArr, dataArr) => {
+    let accumElem = 0;
+    const newArr = origArr.map((elem, idx) => {
+      accumElem += dataArr[idx].distance;
+      elem.distanceAccum = accumElem;
+      elem.distance = dataArr[idx].distance;
+      return elem;
+    });
+    return newArr;
+  };
+
+  // const gpxOutputDataMtn = gpxMtnWorkout
+  //   .data
+  //   .reduce((accum, currElem, idx) => {
+  //     accum.push(currElem);
+  //     currElem.distance = mtnDistance.distArr[idx].distance;
+  //   return accum;
+  // }, []);
+
 
   //Creating Workouts  
   const workouts = await Promise.all([
@@ -82,7 +104,8 @@ async function seed() {
     Workout.create({name: 'Workout #2', description: 'Best workout ever', 
       data: {heartrate: 150, speed: 15}, userId: murphy.id, distance: 0, elevation: 0, time: timeTemplateObj, date: '2022-10-22T13:39:41Z'}),
     Workout.create({name: gpxMtnWorkout.name, description: 'Best workout ever', date: dateMtnWorkout, 
-      data: gpxMtnWorkout.data, userId: murphy.id, distance: mtnDistanceTotal, elevation: mtnElevation, 
+      data: appendToGpxData(gpxMtnWorkout.data, mtnDistance.distArr), userId: murphy.id, distance: mtnDistanceTotal, elevation: mtnElevation, 
+      // data: gpxOutputDataMtn, userId: murphy.id, distance: mtnDistanceTotal, elevation: mtnElevation, 
         time: mtnTime, hrAvg: mtnHrAvg, hrMax: mtnHrMax, cadAvg: mtnCadAvg, cadMax: mtnCadMax, tempAvg: mtnTempAvg,
         speedAvg: mtnSpeedAvg, speedMax: mtnSpeedMax, date: dateMtnWorkout}),
     Workout.create({name: gpxRdWorkout.name, description: 'Best workout ever', date: dateRdWorkout,
@@ -90,7 +113,8 @@ async function seed() {
         time: roadTime, hrAvg: roadHrAvg, hrMax: roadHrMax, cadAvg: roadCadAvg, cadMax: roadCadMax, tempAvg: roadTempAvg,
         speedAvg: roadSpeedAvg, speedMax: roadSpeedMax, date: dateRdWorkout}),
     Workout.create({name: gpxMtnWorkout.name, description: 'Best workout ever', date: dateMtnWorkout,
-      data: gpxMtnWorkout.data, userId: cody.id, distance: mtnDistanceTotal, elevation: mtnElevation, 
+      data: appendToGpxData(gpxMtnWorkout.data, mtnDistance.distArr), userId: cody.id, distance: mtnDistanceTotal, elevation: mtnElevation, 
+      // data: gpxOutputDataMtn, userId: cody.id, distance: mtnDistanceTotal, elevation: mtnElevation, 
         time: mtnTime, hrAvg: mtnHrAvg, hrMax: mtnHrMax, cadAvg: mtnCadAvg, cadMax: mtnCadMax, tempAvg: mtnTempAvg,
         speedAvg: mtnSpeedAvg, speedMax: mtnSpeedMax, date: dateMtnWorkout}),
     Workout.create({name: gpxRdWorkout.name, description: 'Best workout ever', date: dateRdWorkout,
